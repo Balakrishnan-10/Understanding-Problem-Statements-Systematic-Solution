02376d8eee4bcb132d41418b6573693514c9d486
Balakrishnan-10 <krishnabala1495@gmail.com>
2024-12-23 16:04:17 +0530

    differenceOfTwoIntegers

---
+++ practice.js
@@ -0,0 +1,13 @@
+//* 1. Find the difference of two integers :
+
+function differenceOfTwoIntegers(num1, num2) {
+  console.log(num1, num2);
+  num1 = Number(num1);
+  num2 = Number(num2);
+  return num1 - num2;
+}
+console.log(differenceOfTwoIntegers(5, 3));
+console.log(differenceOfTwoIntegers(5, "8"));
+console.log(differenceOfTwoIntegers("12", 3));
+
+//! ---------------------------------------------------------------------
